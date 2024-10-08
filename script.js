5035bda7a5835ed18540f40753893f438f22d691
Mohamad Iqbal <1980193+jacknb1ack@users.noreply.github.com>
2024-10-08 20:57:13 +0700

    add button to create new grid
    fix change bg color only apply on initial grid

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
+//select container class
 const container = document.querySelector(".container");
-<<<<<<< HEAD
+
+//create initial grid 16x16
 createGrid(16);
-function createGrid(input) {
-  const number = input * input;
-  for (let y = 0; y < number; y++) {
-=======
 
-for (let x = 0; x < 16; x++) {
-  const divContainer = document.createElement("div");
-  divContainer.setAttribute("class", "div-container");
-  container.appendChild(divContainer);
-  for (let y = 0; y < 16; y++) {
->>>>>>> parent of daad718 (fix div dimension to fill container)
-    const divBox = document.createElement("div");
-    divBox.setAttribute("class", "div-box");
-    container.appendChild(divBox);
-  }
-}
-
+//add button to reset and create grid with input value
 const button = document.createElement("button");
 button.setAttribute("class", "reset");
 button.textContent = "Create New Grid";
-container.before(button);
 
 button.addEventListener("click", () => {
   const input = prompt("Input grid size (max 100)");
   container.innerHTML = "";
   if (input > 100) {
-    createGrid(100 * 100);
+    createGrid(100);
   } else {
-    createGrid(input * input);
+    createGrid(input);
   }
 });
 
-const action = document.querySelectorAll(".div-box");
-action.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.classList.add("colored");
+//place button before container
+container.before(button);
+
+//create grid
+function createGrid(input) {
+  for (let x = 0; x < input; x++) {
+    const divContainer = document.createElement("div");
+    divContainer.setAttribute("class", "div-container");
+    container.appendChild(divContainer);
+    for (let y = 0; y < input; y++) {
+      const divBox = document.createElement("div");
+      divBox.setAttribute("class", "div-box");
+      divContainer.appendChild(divBox);
+    }
+  }
+
+  //change background color for hovered div-box
+  let action = document.querySelectorAll(".container .div-box");
+  action.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.classList.add("colored");
+    });
   });
-});
+}
